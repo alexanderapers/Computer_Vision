43a5694ed34601b5d9d0cb4e227e14399e6f0f64
alexanderapers <alexander@apers.nl>
2022-02-16 21:04:40 +0100

    feat(main): make screenshot

--- main.cpp
+++ main.cpp
@@ -6,8 +6,16 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <format>
+//#include <cstdint>
 
+//namespace fs = std::filesystem;
 using namespace cv;
+
+String CAMERA_NAME = "Alex_Camera";
 
 int streamWebcamFeed()
 {
@@ -25,6 +33,29 @@ int streamWebcamFeed()
         videoCapture >> frame;
         if (frame.empty()) break;
         cv::imshow("Camera feed", frame);
+
+        // press spacebar for screenshot
+        if (cv::waitKey(0) == 32)
+        {
+            String dirname = std::format("{}_Calibration_Images", CAMERA_NAME);
+            // check if directory exists
+            if (!std::filesystem::exists(dirname))
+            {
+                // if not then make it
+                std::filesystem::create_directory(dirname);
+            }
+
+            // make screenshot and add to directory
+            time_t t = time(0);
+            struct tm now;
+            localtime_s(&now, &t);
+            char buffer[80];
+            
+            String filename = std::format("{0}\\{1}.png", dirname, strftime(buffer, 80, "%Y-%m-%d.", &now));
+            imwrite(filename, frame);
+        }
+
+
         if (cv::waitKey(10) == 27) break;
     }
 
@@ -39,11 +70,11 @@ int main()
     //Mat img = imread(image_path, IMREAD_COLOR);
     //if (img.empty())
     //{
-    //    std::cout << "Could not read the image: " << image_path << std::endl;
+    //    std::cout << "could not read the image: " << image_path << std::endl;
     //    return 1;
     //}
-    //imshow("Display window", img);
-    //int k = waitKey(0); // Wait for a keystroke in the window
+    //imshow("display window", img);
+    //int k = waitKey(0); // wait for a keystroke in the window
     //if (k == 's')
     //{
     //    imwrite("starry_night.png", img);
