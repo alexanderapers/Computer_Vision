5619471aed75a4d4e53f280760115a294347e929
alexanderapers <alexander@apers.nl>
2022-02-18 18:02:20 +0100

    style(main): comments

--- main.cpp
+++ main.cpp
@@ -8,8 +8,16 @@
 int main(int argc, char** argv)
 {
     //ScreenCapture::streamWebcamFeed();
-
+    
+    // get recalibration error of using all images
     CameraCalibration::calibrate(argc, argv);
+
+    // for image in calibimages run calibration without this images
+        // get recalibration error using n - 1 images
+        // if new error < old error:
+            // disregard new
+
+
 
     return 0;
 }
