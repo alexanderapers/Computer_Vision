d5b52ce254a0908f8a952aacdd2ee1c87dde97cd
robertoost <github@robertoo.st>
2022-03-02 00:11:24 +0100

    feat(main): bg subtraction & WIP grid search

--- P2/src/main.cpp
+++ P2/src/main.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 #include <filesystem>
 
-#include "utilities/General.h"
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgproc/types_c.h>
 #include "VoxelReconstruction.h"
+#include "utilities/General.h"
 #include "Video.h"
 
 using namespace nl_uu_science_gmt;
@@ -14,7 +16,7 @@ using namespace std;
 /// <summary>
 /// Calculates the quality measure of two (grayscale) images.
 /// </summary>
-tuple<int, float> quality_measure(Mat img1, Mat img2) {
+float generate_quality_measure(Mat img1, Mat img2) {
 	// Get the total pixel count.
 	int pixel_count = img1.total();
 
@@ -25,12 +27,12 @@ tuple<int, float> quality_measure(Mat img1, Mat img2) {
 
 	// Report quality measure variables.
 	double success = (pixel_count - diff_count) / (float)pixel_count;
-	cout << "Number of differing pixels " << diff_count << endl;
-	cout << "Quality measure: " << success << endl;
-	cout << "Loss: " << 1 - success << endl;
+	//cout << "Number of differing pixels " << diff_count << endl;
+	//cout << "Quality measure: " << success << endl;
+	//cout << "Loss: " << 1 - success << endl;
 
 	// Return the amount of differing pixels, and the success rate.
-	return tuple<int, float>(diff_count, success);
+	return success;
 }
 
 /// <summary>
@@ -53,8 +55,107 @@ void save_frames(string video_path, string output_filename, int n_frames = 1, st
 	}
 }
 
-int main(
-		int argc, char** argv)
+vector<Mat> split_hsv_channels(Mat img) {
+	Mat hsv_img;
+	cvtColor(img, hsv_img, CV_BGR2HSV);  // from BGR to HSV color space
+
+	vector<Mat> img_channels;
+
+	// Split the HSV-channels for further analysis
+	split(hsv_img, img_channels);
+	return img_channels;
+}
+
+/** Copied from Scene3DRenderer
+ * Separate the background from the foreground
+ * ie.: Create an 8 bit image where only the foreground of the scene is white (255)
+ */
+Mat create_foreground(vector<Mat> frame_channels, vector<Mat> bg_channels, int h, int s, int v)
+{
+	// Background subtraction H
+	Mat tmp, foreground, background;
+	absdiff(frame_channels[0], bg_channels[0], tmp);
+	threshold(tmp, foreground, h, 255, CV_THRESH_BINARY);
+
+	// Background subtraction S
+	absdiff(frame_channels[1], bg_channels[1], tmp);
+	threshold(tmp, background, s, 255, CV_THRESH_BINARY);
+	bitwise_and(foreground, background, foreground);
+
+	// Background subtraction V
+	absdiff(frame_channels[2], bg_channels[2], tmp);
+	threshold(tmp, background, v, 255, CV_THRESH_BINARY);
+	bitwise_or(foreground, background, foreground);
+
+	// Improve the foreground image
+	return foreground;
+}
+
+vector<vector<int>> search_algorithm() {
+	vector<vector<int>> cam_HSV_thresholds;
+
+	// For each camera, find the optimal parameters.
+	for (int cam_id = 1; cam_id < 5; cam_id++) {
+		string cam_path = std::format("./data/cam{}/", cam_id);
+		
+		// Take one frame from video.avi
+		VideoCapture cap = VideoCapture(std::format("{}video.avi", cam_path));
+		Mat frame;
+		cap >> frame;
+		
+		// TODO: Get SD & mean of background.
+		Mat background = cv::imread(std::format("{}background.png", cam_path));
+
+		// Get golden standard to compare against
+		Mat golden_standard = cv::imread(std::format("{}golden_standard.png", cam_path), cv::IMREAD_GRAYSCALE);
+
+		// Split the frame and bg into hsv channels for further analysis.
+		vector<Mat> frame_channels = split_hsv_channels(frame);
+		vector<Mat> bg_channels = split_hsv_channels(background);
+
+		// TODO: Insert hyperparameter tuning algorithm here
+		//		 For all channels, generate every possible combination (limit the amount of values to combine).
+
+		// ATTEMPT AT SIMPLE GRID SEARCH ---------------------------------------------------------------- 
+		vector<int> possible_values = { 0, 20, 40, 60, 80, 100, 120 };
+
+		float best_quality_measure = 0;
+		vector<int> best_HSV;
+		// Loop over all combinations of HSV values.
+		for (int H : possible_values) for (int S : possible_values) for (int V : possible_values) {
+
+			Mat foreground = create_foreground(frame_channels, bg_channels, H, S, V);
+
+			float quality_measure = generate_quality_measure(foreground, golden_standard);
+			
+			if (quality_measure > best_quality_measure) {
+				best_quality_measure = quality_measure;
+				best_HSV = {H, S, V};
+			}
+		}
+		// -----------------------------------------------------------------------------------------------
+
+		cam_HSV_thresholds.push_back(best_HSV);
+
+		int H = best_HSV[0];
+		int S = best_HSV[1];
+		int V = best_HSV[2];
+		cout << "Best quality measure " << best_quality_measure << endl;
+		cout << std::format("Camera {0} best HSV values: {1} {2} {3}", cam_id, H, S, V) << endl;
+	}
+	return cam_HSV_thresholds;
+	// For every combination of channel thresholds:
+	// Create a thresholded image using the background image (or mean/sd background image). LOOK AT ProcessForeground function!
+
+	// Compare the thresholded image created by this combination with the gold standard mask using the XOR measure we created.
+	// If the quality measure is higher than the previous combination of parameters, overwrite them with the current ones.
+
+	// After finding a set of parameters that generates the closest quality measure to 1 (minimizing loss):
+	// Update the final HSV (threshold) values in the 3D renderer.
+	// Output parameters.
+}
+
+int main(int argc, char** argv)
 {
 	//// GETS FRAMES FROM INTRINSICS VIDEO AND SAVES THEM INTO FILE
 	//save_frames(General::IntrinsicsVideo, "frame", 50, "intrinsics");
@@ -70,14 +171,16 @@ int main(
 		const std::string output_file_path = std::format("./data/cam{}/", i) + General::IntrinsicsFile;
 		General::writeIntrinsics(input_file_path, output_file_path);
 	}*/
+
+	auto results = search_algorithm();
 	
 	//VoxelReconstruction::showKeys();
 	//VoxelReconstruction vr("data" + std::string(PATH_SEP), 4);
 	//vr.run(argc, argv);
 
-	Mat golden_standard = cv::imread("./data/cam1/golden_standard.png", cv::IMREAD_GRAYSCALE);
-	Mat two_pixels_off = cv::imread("./data/cam1/two_pixels_off.png", cv::IMREAD_GRAYSCALE);
-	auto measure_values = quality_measure(golden_standard, two_pixels_off);
+	//Mat golden_standard = cv::imread("./data/cam1/golden_standard.png", cv::IMREAD_GRAYSCALE);
+	//Mat two_pixels_off = cv::imread("./data/cam1/two_pixels_off.png", cv::IMREAD_GRAYSCALE);
+	//auto measure_values = quality_measure(golden_standard, two_pixels_off);
 
 	while (true) {}
 	return EXIT_SUCCESS;	
