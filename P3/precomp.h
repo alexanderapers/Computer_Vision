47c34c70c193219d1533f4e960c4c7645bf6ff17
alexanderapers <alexander@apers.nl>
2022-03-10 00:42:19 +0100

    chore(precomp): added ml.hpp

--- P3/precomp.h
+++ P3/precomp.h
@@ -58,6 +58,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgproc/types_c.h>
+#include <opencv2/ml.hpp>
 
 // ===========================================
 // NAMESPACES
