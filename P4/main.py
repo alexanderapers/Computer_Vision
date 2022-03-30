d4a8890bed7b422cc7eb536f6c82e46ced0f59b5
robertoost <github@robertoo.st>
2022-03-30 17:01:37 +0200

    feat(main): train all models

--- P4/main.py
+++ P4/main.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 # TO DISABLE USE OF THE GPU, UNCOMMENT THIS LINE
@@ -12,28 +13,28 @@ from keras.utils.np_utils import to_categorical
 from keras.callbacks import History
 from keras import Sequential
 
+from typing import Callable
+
 import plotting
 import models
 import metrics
-
-from typing import Callable
+from learning_rate_scheduler import scheduler
 
 # Global variables
 NUM_CLASSES = 10
-NUM_K_FOLDS = 2
-LEARNING_RATE = 0.001
+NUM_K_FOLDS = 10
 BATCH_SIZE = 32
-EPOCHS = 1
+EPOCHS = 10
 
 CLASS_NAMES = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 all_models: dict[str, Callable[[], Sequential]] = {
     'Baseline': models.get_baseline,
-    'Variant Learning Rate': models.get_variant_learning_rate,
-    'Variant Dropout': models.get_variant_dropout,
-    'Variant Softpool': models.get_variant_softpool,
-    'Variant Leaky ReLU': models.get_variant_activation
+    'Variant_Learning Rate': models.get_variant_learning_rate,
+    'Variant_Dropout': models.get_variant_dropout,
+    'Variant_Softpool': models.get_variant_softpool,
+    'Variant_Leaky_ReLU': models.get_variant_activation
 }
 
 def main():
@@ -44,28 +45,22 @@ def main():
     x_train = x_train.astype('float32') / 255.0
     x_test = x_test.astype('float32') / 255.0
 
-    # # do we need to shuffle the data before? is ti ordered on label?
-    # x_train, x_validation, y_train, y_validation = train_test_split(
-    #                 x_train, y_train, test_size = 0.2, random_state = 0, shuffle = True)
-
-    #print(x_validation.shape)
-
     # Convert the labels using one-hot encoding.
     y_train = to_categorical(y_train, num_classes=NUM_CLASSES)
-    # y_validation = to_categorical(y_validation, num_classes=NUM_CLASSES)
     y_test = to_categorical(y_test, num_classes=NUM_CLASSES)
 
     # K-Fold cross validation setup
     k_fold = KFold(n_splits=NUM_K_FOLDS, shuffle=True, random_state=42)
 
     timeline : list[dict] = []
-    model_metrics = {}
+    final_metrics = {}
 
     for model_label, model_getter in all_models.items():
         print(f"\nStarting K={NUM_K_FOLDS}-fold validation of {model_label}")
-        
-        fold = 1
+
+        fold = 0
         for train_indices, validation_indices in k_fold.split(x_train):
+            fold += 1
             print(f"\nFold {fold}/{NUM_K_FOLDS}\n")
 
             # Get training data from split for this fold.
@@ -77,23 +72,28 @@ def main():
             # Redefine the model for the current fold.
             model = model_getter()
             
+
+
             # Train and validate the model.
             history : History = model.fit(fold_x_train, fold_y_train, 
                 validation_data=(fold_x_validation, fold_y_validation), 
                 batch_size=BATCH_SIZE, epochs=EPOCHS)
             
+            # If this is the last fold, save the model weights
+            if fold == NUM_K_FOLDS:
+                checkpoint_path = f"./training_weights/{model_label}.ckpt"
+                print(f"\nSaving weights for {model_label}")
+                model.save_weights(checkpoint_path)
+
             timeline.append(history.history)
 
         metrics_history = metrics.model_metrics(timeline, ['accuracy', 'loss', 'val_loss', 'val_accuracy'])
-        model_metrics = 
         plotting.plot_mean_metric(metrics_history['accuracy'], metrics_history['val_accuracy'], model_label, 'accuracy')
         plotting.plot_mean_metric(metrics_history['loss'], metrics_history['val_loss'], model_label, 'loss')
 
-    
+        final_metrics[model_label] = (metrics_history['accuracy']['mean'][-1], metrics_history['val_accuracy']['mean'][-1])
 
-    # plotting.plot_mean_metric(mean_loss, mean_val_loss, 'Baseline', 'loss')
-    # plotting.plot_mean_metric(mean_accuracy, mean_val_accuracy, 'Baseline', 'accuracy')
-
+    metrics.store_final_accuracy(final_metrics)
     # # Create and validate final model using all training data.ds
     # model = get_model()
     # opt = keras.optimizers.Adam(learning_rate=LEARNING_RATE)
