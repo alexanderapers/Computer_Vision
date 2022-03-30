20b13085c2643f347965977fd7133d2dbcb96005
robertoost <github@robertoo.st>
2022-03-30 17:00:55 +0200

    feat(metrics): store final accuracy scores

--- P4/metrics.py
+++ P4/metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import csv
 
 def calculate_metric_mean_std(timeline, metric_label):
     metric = [history[metric_label] for history in timeline]
@@ -17,3 +18,15 @@ def model_metrics(timeline, labels):
         }
 
     return metrics_dict
+
+def store_final_accuracy(final_metrics):
+    # open file for writing, "w" is writing
+    w = csv.writer(open("output.csv", "w"))
+
+    w.writerow(['Model', 'Mean training accuracy', 'Mean validation accuracy'])
+
+    # loop over dictionary keys and values
+    for model_label, accuracies in final_metrics.items():
+        training, validation = accuracies
+        # write every key and value to file
+        w.writerow([model_label, training, validation])
