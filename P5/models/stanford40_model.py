34622287181f7c642c436ef91597d03a6ab120e3
robertoost <github@robertoo.st>
2022-04-07 19:59:20 +0200

    feat(stanford40_model): define s40 model

---
+++ P5/models/stanford40_model.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers, Sequential
+
+def get_model():
+    model_layers = [
+        layers.InputLayer(input_shape=(224, 224, 3)),
+        
+        # Conv1
+        layers.Conv2D(filters=96, kernel_size=7, strides=2, padding="same", activation='relu'),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3, 3), strides=2, padding="same"),
+
+        # Conv2
+        layers.Conv2D(filters=256, kernel_size=5, strides=2, padding="same", activation='relu'),
+        layers.BatchNormalization(),
+        layers.MaxPool2D(pool_size=(3, 3), strides=2, padding="same"),
+
+        # Conv3
+        layers.Conv2D(filters=512, kernel_size=3, strides=1, padding="same", activation='relu'),
+        # Conv4
+        layers.Conv2D(filters=512, kernel_size=3, strides=1, padding="same", activation='relu'),
+
+        # Conv5
+        layers.Conv2D(filters=512, kernel_size=3, strides=1, padding="same", activation='relu'),
+        layers.MaxPool2D(pool_size=(3, 3), strides=2, padding="same"),
+        layers.Flatten(),
+
+
+        # Full6
+        layers.Dense(4096),
+        layers.Dropout(rate=0.5),
+
+        # Full7
+        layers.Dense(2048),
+        layers.Dropout(rate=0.5),
+
+        # Output
+        # TODO: Use NUM_CLASSES value.
+        layers.Dense(40)
+    ]
+
+    model = Sequential(model_layers)
+    opt = keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer=opt,
+            loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy'])
+
+    return model
+
+
