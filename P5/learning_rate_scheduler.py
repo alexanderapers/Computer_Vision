6343c4f6e21844571a7f0e1c65ba9d15d6c90dea
robertoost <github@robertoo.st>
2022-04-15 18:10:41 +0200

    feat(stanford40): reach 20%

--- P5/learning_rate_scheduler.py
+++ P5/learning_rate_scheduler.py
@@ -1,5 +1,5 @@
 def halving_scheduler(epoch, lr):
-  if epoch % 5 == 0 and epoch != 0:
+  if epoch % 4 == 0 and epoch != 0:
     print(f"We've halved the lr to be {0.5 * lr}")
     return 0.5 * lr
   else:
