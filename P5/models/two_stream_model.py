1fb730c453c156d6c21e9177c0aa017e2f4dd2ed
robertoost <github@robertoo.st>
2022-04-19 02:26:59 +0200

    chore(two_stream_model): remove comment

--- P5/models/two_stream_model.py
+++ P5/models/two_stream_model.py
@@ -107,19 +107,3 @@ def test_model():
 
     print(f"\nTesting epoch {CHOSEN_EPOCH}...")
     loss, acc = model.evaluate(test_dataset, verbose=1)
-
-
-# Load both models
-# Load both model weights
-# Set all model weights to trainable=false
-
-# Take the output of both model's flatten layers, and then combine those.
-# It's also an option to keep a pretrained dense layer, since this time we're working with the same classes.
-
-# keras has a concatenate/merge api layer thingie. Look into that.
-
-# Then find a way to change our input to fit the new 2-stream model.
-
-# train until the graphs look nice.
-# test and record test score.
-# Make sure training accuracy is above 30% before we choose a testing candidate
